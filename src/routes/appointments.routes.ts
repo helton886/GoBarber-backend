1ce2ec01a07efb1c737ede879859b0c66ad6ee91
Helton Vasconcelos <heltonvasconcelos88@gmail.com>
2020-11-13 00:45:23 -0300

    chore: SCO and DTO implemented

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -5,6 +5,11 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+  return response.send(appointments);
+});
+
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
@@ -17,10 +22,13 @@ appointmentsRouter.post('/', (request, response) => {
   if (findAppointmentInSameDate) {
     return response
       .status(400)
-      .json({ message: 'Thi appointment is already booked' });
+      .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = appointmentsRepository.create(provider, parsedDate);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   return response.json({ appointment });
 });
