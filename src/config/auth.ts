9765771beb13d69065751bda7736829f4360d844
Helton Vasconcelos <heltonvasconcelos88@gmail.com>
2020-11-30 23:47:41 -0300

    feature: Auth user

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'efe0fd3e7568ffb7c9cc6e9cbcbab81b',
+    expiresIn: '1d',
+  },
+};
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,31 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+import User from '../models/User';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export default class CreateUserService {
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+    const UserExists = await usersRepository.findOne({ where: { email } });
+
+    if (UserExists) {
+      throw new Error('Email already used');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await usersRepository.save(user);
+    return user;
+  }
+}
