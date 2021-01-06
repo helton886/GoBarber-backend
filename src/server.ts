f93337cdc3f64bfa0f7c8fda709bf7b2a0b4658a
Helton Vasconcelos <heltonvasconcelos88@gmail.com>
2021-01-06 15:08:49 -0300

    chore: enable cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 import 'reflect-metadata';
@@ -10,6 +11,7 @@ import './database';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
