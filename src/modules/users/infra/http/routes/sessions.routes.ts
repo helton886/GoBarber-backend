6c4ad48f2a8eb9c15d228fbcdb75a7240563c9d5
Helton Vasconcelos <heltonvasconcelos88@gmail.com>
2021-01-11 03:26:10 -0300

    changed files to use controllers

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,20 +1,10 @@
 import { Router } from 'express';
-import { container } from 'tsyringe';
-import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import SessionsController from '../controllers/sessionsController';
 
 const usersRouter = Router();
 
-usersRouter.post('/', async (request, response) => {
-  const { email, password } = request.body;
-  const authenticateUser = container.resolve(AuthenticateUserService);
+const sessionsController = new SessionsController();
 
-  const { user, token } = await authenticateUser.execute({
-    email,
-    password,
-  });
-  delete user.password;
-
-  return response.json({ user, token });
-});
+usersRouter.post('/', sessionsController.create);
 
 export default usersRouter;
